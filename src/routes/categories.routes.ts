76fb078583215c3caa5fb67ea6cb170b541dfac4
Willian Del Freu <williankeyex@gmail.com>
2021-07-05 22:19:02 -0300

    chore: avatar user update delete

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,15 +1,14 @@
 import { Router } from "express";
 import multer from "multer";
 
+import uploadConfig from "../config/upload";
 import { CreateCategoryController } from "../modules/cars/useCases/createCategory/CreateCategoryController";
 import { ImportCategoryController } from "../modules/cars/useCases/importCategory/ImportCategoryController";
 import { ListCategoriesController } from "../modules/cars/useCases/listCategories/ListCategoriesController";
 
 const categoriesRoutes = Router();
 
-const upload = multer({
-  dest: "./tmp",
-});
+const uploadCategories = multer(uploadConfig.upload("./tmp/"));
 
 const createCategoryController = new CreateCategoryController();
 const importCategoryController = new ImportCategoryController();
@@ -21,7 +20,7 @@ categoriesRoutes.get("/", listCategoriesController.handle);
 
 categoriesRoutes.post(
   "/import",
-  upload.single("file"),
+  uploadCategories.single("categories"),
   importCategoryController.handle
 );
 
